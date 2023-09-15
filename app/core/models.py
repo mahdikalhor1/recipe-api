ff86b29f3abbe47b68d3b88546aefdd45ab91b81
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-09-15 20:45:55 +0330

    implement user_model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserMananger(BaseUserManager):
+
+    def create(self, email, password=None, **extra_fields):
+
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=300, unique=True)
+    name = models.CharField(max_length=300)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD='email'
+
+    objects = UserMananger()
