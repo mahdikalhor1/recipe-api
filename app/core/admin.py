bdecb551d6f983a33b0a0a01efac175decb7acc1
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-09-16 17:25:26 +0330

    implement users update by admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,6 +6,14 @@ class UserModelAdmin(UserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
 
+    fieldsets=(
+        (None, {'fields' : ('email', 'name')}),
+        ('permissions', {'fields' : ('is_active', 'is_staff', 'is_superuser')}),
+        ('dates', {'fields' : ('last_login',)}),
+    )
+
+    readonly_fields=['last_login']
+
 
 admin.site.register(models.User, UserModelAdmin)
 
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -9,7 +9,7 @@ class TestAdmin(TestCase):
 
     def setUp(self):
         """initializing requirements of testing"""
-        
+
         self.client = Client()
         self.superuser = get_user_model().objects.create_superuser(
             email='admin@example.com',
@@ -29,5 +29,15 @@ class TestAdmin(TestCase):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
-        self.assertEqual(res, self.user.email)
-        self.assertEqual(res, self.user.name)
+        self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+
+
+    def test_update_user(self):
+        """checking that update user page works"""
+
+        url = reverse('admin:core_user_change', args=[self.user.id])
+
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
