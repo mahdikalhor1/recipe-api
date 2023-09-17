9e46f5b83f1110e0f406da27a691b7c9c5487716
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-09-17 15:25:50 +0330

    fix bugs

--- app/app/settings.py
+++ app/app/settings.py
@@ -132,5 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL='core.User'
 
 REST_FRAMEWORK={
-    'DEFAULT_SCHEMA_CLASS':'drf_spectacular.openapi.AuthoSchema',
+    'DEFAULT_SCHEMA_CLASS':'drf_spectacular.openapi.AutoSchema',
 }
