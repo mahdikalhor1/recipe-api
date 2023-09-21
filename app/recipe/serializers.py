648174a95a326d7e1c6f38e2c65eea42601e9b07
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-09-21 11:28:35 +0330

    implement recipe detail

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,8 +6,13 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields='__all__'
+        fields=['id', 'title', 'price', 'time_minute', 'link']
         read_only_fields=['id']
+
+class RecipeDetailSerializer(RecipeSerializer):
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
 
 
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@
 
 
 from rest_framework import viewsets
-from .serializers import RecipeSerializer
+from .serializers import RecipeSerializer, RecipeDetailSerializer
 from core.models import Recipe
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -10,7 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 class RecipeManageView(viewsets.ModelViewSet):
     """view for managing recipe objects"""
 
-    serializer_class = RecipeSerializer
+    serializer_class = RecipeDetailSerializer
     authentication_classes=[TokenAuthentication]
     permission_classes=[IsAuthenticated]
 
@@ -20,4 +20,9 @@ class RecipeManageView(viewsets.ModelViewSet):
         """return recipes queryset for authenticated user"""
         return Recipe.objects.filter(user=self.request.user).order_by('-id')
     
-    
+    def get_serializer_class(self):
+        
+        if self.action == 'list':
+            return RecipeSerializer
+
+        return self.serializer_class
